33745cd832b5681c94a8975905f1e1022fd2cca9
Keith Gautreaux <keith.gautreaux@gmail.com>
2012-09-07 10:54:20 -0500

    * Added some attributes_test cases.

--- test/pillboxr/attributes_test.rb
+++ test/pillboxr/attributes_test.rb
@@ -13,4 +13,19 @@ class TestAttributes < MiniTest::Unit::TestCase
   def test_fetching_an_api_attribute
     assert_equal(:color, api_attributes.fetch(:splcolor))
   end
+
+  def test_to_param
+    Pillboxr::Attributes::COLORS.each do |k,v|
+      color = Pillboxr::Attributes::Color.new(k)
+      assert_equal("&color=#{v}", color.to_param)
+    end
+    Pillboxr::Attributes::SHAPES.each do |k,v|
+      shape = Pillboxr::Attributes::Shape.new(k)
+      assert_equal("&shape=#{v}", shape.to_param)
+    end
+    Pillboxr::Attributes::DEA_CODES.each do |k,v|
+      code = Pillboxr::Attributes::Schedule.new(k)
+      assert_equal("&dea=#{v}", code.to_param)
+    end
+  end
 end
